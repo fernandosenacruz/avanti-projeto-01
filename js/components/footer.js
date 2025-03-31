f9c792e03db87c60db1fb89eb59bda3f16703e3d
Fernando Sena <fernando.sena@mkdata.com.br>
2025-03-31 16:39:21 -0300

    feat: cria footer

---
+++ js/components/footer.js
@@ -0,0 +1,26 @@
+export function createFooter() {
+  const footer = document.createElement('footer');
+  footer.className = 'footer container';
+  footer.innerHTML = `
+    <p class="text-center mx-3">
+      Lorem ipsum dolor sit amet, consectetuer adipiscing elit,
+      sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. 
+      Ut wisi enim ad minim veniam, quis nostrud exerci.
+    </p>
+    <div class="row text-center mx-5">
+      <div class="col-3">
+        <img src="/assets/lets_encrypt.png" alt="logo let's encrypt" class="img-fluid" />
+      </div>
+      <div class="col-3">
+        <img src="/assets/vtex-pci-200.png" alt="logo vtex certificado" class="img-fluid" />
+      </div>
+      <div class="col-3">
+        <img src="/assets/avanti.png" alt="logo avanti" class="img-fluid" />
+      </div>
+      <div class="col-3">
+        <img src="/assets/VTEX_Logo.png" alt="logo VTEX" class="img-fluid" />
+      </div>
+    </div>
+  `;
+  return footer;
+}
