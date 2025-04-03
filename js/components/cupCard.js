4dc734334af84fcbad76973cc522c563748b90b0
Fernando Sena <fernando.sena@mkdata.com.br>
2025-04-03 12:09:21 -0300

    feat: cria card de caneca avanti

---
+++ js/components/cupCard.js
@@ -0,0 +1,26 @@
+export function createCupCard() {
+  const card = document.createElement('div');
+  card.innerHTML = `
+  <div class="card border-0 justify-content-center align-items-center">
+    <div class="card-image m-0 p-0 cup-card-img">
+      <img src="/assets/cup-avanti.png" alt="Cup avanti" class="card-img-top"/>
+    </div>
+    <div class="card-body m-2">
+      <h4 class="card-title">Lorem Ipsum</h4>
+      <p class="card-text">
+        Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. 
+        Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim.
+      </p>
+      <p class="card-text">
+        Cras dignissim est et pellentesque tincidunt. Praesent bibendum quis velit a aliquam. 
+        Ut vestibulum turpis eget mi iaculis ullamcorper. 
+        Curabitur nec metus sed tortor sollicitudin porta nec eu enim. 
+        Ut fermentum scelerisque tortor mollis volutpat. 
+        Mauris iaculis magna nisl, vel porttitor augue placerat et.
+      </p>
+    </div>
+  </div>
+  `;
+
+  return card;
+}
--- js/main.js
+++ js/main.js
@@ -2,6 +2,7 @@ import { createDiscountCoupon } from './components/discountCoupon.js';
 import { createHeader } from './components/header.js';
 import { createBanner } from './components/banner.js';
 import { createCarrousel } from './components/carrousel.js';
+import { createCupCard } from './components/cupCard.js';
 import { createFooter } from './components/footer.js';
 
 document.addEventListener('DOMContentLoaded', () => {
@@ -12,5 +13,6 @@ document.addEventListener('DOMContentLoaded', () => {
   app.appendChild(createHeader());
   app.appendChild(createBanner());
   app.appendChild(createCarrousel());
+  app.appendChild(createCupCard());
   app.appendChild(createFooter());
 });
